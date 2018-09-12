1523b44b8d27fb519e63695734c06fd2e60241e2
PowerDogiwe <42038911+PowerDogiwe@users.noreply.github.com>
2018-09-12 10:37:34 +0300

    initial commit

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1536736083,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/clickbttn-sheet0.png",
+		"images/staffbttn-sheet0.png",
+		"images/autoworkbttn-sheet0.png",
+		"images/coffeebttn-sheet0.png",
+		"images/backbttn-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
